d32d2401ee59021bbdd5622a4a7b997255720a64
Dhyey Sapara <dhyey@simformsolutions.com>
2023-06-06 14:06:51 +0530

    Controller Test for User Controller

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -6,44 +6,27 @@ class UsersControllerTest < ActionDispatch::IntegrationTest
     @user = users(:one)
   end
 
-  test "should get index" do
-    get users_url
-    assert_response :success
-  end
-
-  test "should get new" do
-    get new_user_url
+  test "should get user registeration" do
+    get new_user_registration_path
     assert_response :success
   end
 
   test "should create user" do
     assert_difference("User.count") do
-      post users_url, params: { user: {  } }
+      post user_registration_url, params: { user: { username: "bob", email: "bob@gmail.com", password: "112233" } }
     end
 
-    assert_redirected_to user_url(User.last)
+    assert_redirected_to root_url
   end
 
-  test "should show user" do
-    get user_url(@user)
+  test "should get user login" do
+    get new_user_session_path
     assert_response :success
   end
 
-  test "should get edit" do
-    get edit_user_url(@user)
-    assert_response :success
-  end
-
-  test "should update user" do
-    patch user_url(@user), params: { user: {  } }
-    assert_redirected_to user_url(@user)
-  end
-
-  test "should destroy user" do
-    assert_difference("User.count", -1) do
-      delete user_url(@user)
-    end
-
-    assert_redirected_to users_url
+  test "should sign out user" do
+    sign_in(@user)
+    delete destroy_user_session_url
+    assert_redirected_to root_path
   end
 end
